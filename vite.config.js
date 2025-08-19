a84261acfd611ed410535c7a985574bd3c6b3daf
B-Gerb <gerber.br@northeastern.edu>
2025-08-18 20:14:40 -0400

    For github pages

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ballWebsite/', // Replace with your repository name
 })
